0f43c389737ca6c7b397f9fa071b0f67952b8194
brayanAdmonGas <brayan.ac@admongas.com.mx>
2024-10-10 16:27:17 -0600

    reporte-sasisopa

--- public/reporte-sasisopa/sasisopa-elemento1.php
+++ public/reporte-sasisopa/sasisopa-elemento1.php
@@ -5,17 +5,18 @@ $FechaInicio = date('Y-m-d', $_GET['FechaInicio']);
 $FechaTermino = date('Y-m-d', $_GET['FechaTermino']);
 
 ?>
-<a class="float-right" onclick="DescargarPolitica(<?=$Session_IDEstacion;?>)" style="cursor: pointer;" data-toggle="tooltip" data-placement="left" title="Descargar 1. Politica" >
-Descargar politica <img src="<?php echo RUTA_IMG_ICONOS."pdf.png"; ?>">
+<div class="text-end">
+<a onclick="DescargarPolitica(<?= $Session_IDEstacion; ?>)" style="cursor: pointer;" data-toggle="tooltip" data-placement="left" title="Descargar 1. Politica">
+<img src="<?php echo RUTA_IMG_ICONOS . "pdf.png"; ?>">
 </a>
-
+</div>
 <h4>1. POLÍTICA</h4>
-<?php 
-$sql1 = "SELECT * FROM tb_politica_lista_comprobacion WHERE id_estacion = '".$Session_IDEstacion."' AND fecha BETWEEN '".$FechaInicio."' AND '".$FechaTermino."' ORDER BY fecha DESC ";
+<?php
+$sql1 = "SELECT * FROM tb_politica_lista_comprobacion WHERE id_estacion = '" . $Session_IDEstacion . "' AND fecha BETWEEN '" . $FechaInicio . "' AND '" . $FechaTermino . "' ORDER BY fecha DESC ";
 $result1 = mysqli_query($con, $sql1);
 $numero1 = mysqli_num_rows($result1);
 
-$sql2 = "SELECT * FROM tb_lista_asistencia WHERE id_estacion = '".$Session_IDEstacion."' AND punto_sasisopa = 1 AND fecha BETWEEN '".$FechaInicio."' AND '".$FechaTermino."' ";
+$sql2 = "SELECT * FROM tb_lista_asistencia WHERE id_estacion = '" . $Session_IDEstacion . "' AND punto_sasisopa = 1 AND fecha BETWEEN '" . $FechaInicio . "' AND '" . $FechaTermino . "' ";
 $result2 = mysqli_query($con, $sql2);
 $numero2 = mysqli_num_rows($result2);
 
@@ -34,20 +35,19 @@ echo '<div class="row">
 <tbody>';
 $num = 1;
 if ($numero1 > 0) {
-while($row1 = mysqli_fetch_array($result1, MYSQLI_ASSOC)){
-$id = $row1['id'];
+    while ($row1 = mysqli_fetch_array($result1, MYSQLI_ASSOC)) {
+        $id = $row1['id'];
 
-echo '<tr>
-<td class="text-center">'.$num.'</td>
-<td class="text-center">'.FormatoFecha($row1['fecha']).'</td>
-<td class="text-center align-middle" width="30"><img src="'.RUTA_IMG_ICONOS.'pdf.png" style="cursor: pointer;" onclick="DescargarRegistro('.$id.')"></td>
+        echo '<tr>
+<td class="text-center">' . $num . '</td>
+<td class="text-center">' . FormatoFecha($row1['fecha']) . '</td>
+<td class="text-center align-middle" width="30"><img src="' . RUTA_IMG_ICONOS . 'pdf.png" style="cursor: pointer;" onclick="DescargarRegistro(' . $id . ')"></td>
 </tr>';
 
-$num = $num + 1;
-}
-}else{
-echo '<td colspan="3" class="text-center text-secondary" style="font-size: .8em;">No se encontró información para mostrar</td>';
-
+        $num = $num + 1;
+    }
+} else {
+    echo '<td colspan="3" class="text-center text-secondary" style="font-size: .8em;">No se encontró información para mostrar</td>';
 }
 echo '</tbody></table>';
 
@@ -69,24 +69,23 @@ echo '<div class="col-6">
 
 $num1 = 1;
 if ($numero2 > 0) {
-while($row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)){
-$id1 = $row2['id'];
-$estado = $row2['estado'];
+    while ($row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)) {
+        $id1 = $row2['id'];
+        $estado = $row2['estado'];
 
-echo "<tr>
-<td class='text-center'>".$num1."</td>
-<td class='text-center'>".FormatoFecha($row2['fecha'])."</td>
-<td class='text-center'>".date('g:i a', strtotime($row2['hora']))."</td>
-<td class='text-center align-middle' width='30'><img src='".RUTA_IMG_ICONOS."pdf.png' style='cursor: pointer;' onclick='DescargarAsistencia(".$id1.")'></td>
+        echo "<tr>
+<td class='text-center'>" . $num1 . "</td>
+<td class='text-center'>" . FormatoFecha($row2['fecha']) . "</td>
+<td class='text-center'>" . date('g:i a', strtotime($row2['hora'])) . "</td>
+<td class='text-center align-middle' width='30'><img src='" . RUTA_IMG_ICONOS . "pdf.png' style='cursor: pointer;' onclick='DescargarAsistencia(" . $id1 . ")'></td>
 </tr>";
 
-$num1 = $num1 + 1;
+        $num1 = $num1 + 1;
+    }
+} else {
+    echo "<td colspan='8' class='text-center text-secondary' style='font-size: .8em;'>No se encontró información para mostrar</td>";
 }
-}else{
-echo "<td colspan='8' class='text-center text-secondary' style='font-size: .8em;'>No se encontró información para mostrar</td>";
 
-}
- 
 echo '</tbody></table>';
 
 echo '</div>';
